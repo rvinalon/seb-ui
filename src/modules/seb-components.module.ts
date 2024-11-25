ce09a5606d4272fc1cca050a65e61cb58405f452
Towambii Vinalon <vinalon.ronald@gmail.com>
2024-11-25 19:30:00 +0100

    add more import

--- src/modules/seb-components.module.ts
+++ src/modules/seb-components.module.ts
@@ -1,23 +1,33 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { NggCoreWrapperModule } from '@sebgroup/green-angular';
+import { FormsModule } from '@angular/forms';
+
+//Components
 import '@sebgroup/green-core/components/button/button.js';
 import '@sebgroup/green-core/components/input/input.js';
-import '@sebgroup/green-core/components/icon/icon.js';
 import '@sebgroup/green-core/components/link/link.js';
 import '@sebgroup/green-core/components/card/card.js';
 import '@sebgroup/green-core/components/rich-text/rich-text.js';
 import '@sebgroup/green-core/components/theme/theme.js';
 import '@sebgroup/green-core/components/fab/fab.js';
-import '@sebgroup/green-core/components/fab/fab.js';
 import '@sebgroup/green-core/components/signal/signal.js';
 
+//Layout
+import '@sebgroup/green-core/components/flex/flex.js';
+import '@sebgroup/green-core/components/grid/grid.js';
 
-
+//Icons
+import '@sebgroup/green-core/components/icon/icon.js';
+import '@sebgroup/green-core/components/icon/icons/people-profile.js';
+import '@sebgroup/green-core/components/icon/icons/robot.js';
+import '@sebgroup/green-core/components/icon/icons/brand-seb.js';
+import '@sebgroup/green-core/components/icon/icons/play.js';
 
 
 
 @NgModule({
     imports: [
+        FormsModule,
         NggCoreWrapperModule
     ],
     exports: [
--- src/pages/voice-chat/voice-chat.component.ts
+++ src/pages/voice-chat/voice-chat.component.ts
@@ -1,14 +1,27 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { SebComponentModule } from '../../modules/seb-components.module';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-voice-chat',
-  imports: [ SebComponentModule ],
+  imports: [SebComponentModule, CommonModule],
   templateUrl: './voice-chat.component.html',
   styleUrl: './voice-chat.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class VoiceChatComponent {
+  @ViewChild('promptInput') promptInput!: ElementRef;
 
+  onKey(event: KeyboardEvent) {
+    const promptValue = (this.promptInput?.nativeElement?.value ?? '')
+    if (event.key === 'Enter' && promptValue?.length > 1) {
+      console.log('Enter key pressed!', promptValue);
+    }
+  }
+
+  get hasPrompt() {
+    const value = this.promptInput?.nativeElement?.value?.length ?? 0;
+    return value > 1;
+  }
 }
